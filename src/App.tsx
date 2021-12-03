61cf8a4c88838372d8c9bfec7d3057c84af95b12
= <alexandressoares383@gmail.com>
2021-12-03 20:29:24 -0300

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import Header from "./components/Header";
 import { GlobaStyle } from "./styles/global";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
       <GlobaStyle />
-    </div>
+    </>
   );
 }
 
