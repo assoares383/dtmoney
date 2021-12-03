f898b7677235ccd60013534863f4bade374021e4
= <alexandressoares383@gmail.com>
2021-12-03 20:36:37 -0300

    feat: dashboard component and summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+const Summary = () => {
+    return (
+        <Container>
+            <h1>Summary</h1>
+        </Container>
+    )
+}
+
+export default Summary;
