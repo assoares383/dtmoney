77834a578eb6c11322eef97a7d89a3f7896f99e7
= <alexandressoares383@gmail.com>
2021-12-04 19:52:10 -0300

    feat:  summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+
 import { GlobaStyle } from "./styles/global";
 
 function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobaStyle />
     </>
   );
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,33 @@
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
 import { Container } from './styles';
 
 const Summary = () => {
     return (
         <Container>
-            <h1>Summary</h1>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>R$1000,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saidas</p>
+                    <img src={outcomeImg} alt="Saidas" />
+                </header>
+                <strong>- R$5000,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>R$1000,00</strong>
+            </div>
         </Container>
     )
 }
